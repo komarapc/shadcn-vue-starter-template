54cd3ae39ea2ccc5b9b91a25350894f5e8bef54a
izmi.komar <komar.izmi@gmail.com>
2024-06-27 02:01:20 +0700

    refactor: Update 404 page with improved layout and SEO meta tags

--- src/pages/404.vue
+++ src/pages/404.vue
@@ -1,14 +1,38 @@
 <script setup lang="ts">
-
+import { useThemeStore } from "@/stores";
+import { useRouter } from "vue-router";
+import { Button } from "@/components/ui/button";
+import imageNotFound from "@/assets/img/404.png";
+import { useSeoMeta } from "@unhead/vue";
+const router = useRouter();
+const theme = useThemeStore();
+useSeoMeta({
+  title: "404 Not Found",
+  description: "The page you are looking for does not exist.",
+  ogTitle: "404 Not Found",
+  ogDescription: "The page you are looking for does not exist.",
+});
 </script>
 
 <template>
-  <div>
-    <h1>404</h1>
-    <p>Page not found</p>
+  <div
+    :class="[
+      theme.darkMode ? 'dark' : '',
+      'bg-background text-foreground/60 w-full min-h-screen',
+      'flex flex-col items-center justify-center',
+    ]"
+  >
+    <div class="flex flex-col items-center justify-center my-auto align-middle">
+      <img :src="imageNotFound" alt="404" class="w-96 h-96" />
+
+      <div class="mt-10 text-center">
+        <p class="text-center mt-2">
+          The page you are looking for does not exist.
+        </p>
+        <Button @click="router.push('/')" class="mt-4">Go back to home </Button>
+      </div>
+    </div>
   </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
