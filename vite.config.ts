7006ca956fd270576df758dc2c897923f2678387
izmi.komar <komar.izmi@gmail.com>
2024-06-27 02:01:14 +0700

    chore: Add vite-svg-loader plugin for SVG loading

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
 import vueDevTools from "vite-plugin-vue-devtools";
+import svgLoader from "vite-svg-loader";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
+  plugins: [vue(), vueDevTools(), svgLoader()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
