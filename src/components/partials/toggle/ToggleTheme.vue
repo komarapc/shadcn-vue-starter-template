4c03cd60504478fe659f6fd3d198d6de2bdb4ca1
izmi.komar <komar.izmi@gmail.com>
2024-08-13 22:23:57 +0700

    refactor: Update ToggleTheme.vue to use single quotes for imports and consistent formatting

--- src/components/partials/toggle/ToggleTheme.vue
+++ src/components/partials/toggle/ToggleTheme.vue
@@ -1,22 +1,22 @@
 <script setup lang="ts">
-import { useThemeStore } from "@/stores";
-import { computed, watchEffect } from "vue";
+import { useThemeStore } from '@/stores';
+import { computed, watchEffect } from 'vue';
 
 const theme = useThemeStore();
 const themeIcon = computed(() =>
   theme.darkMode
-    ? "line-md:moon-twotone-loop"
-    : "line-md:moon-filled-alt-to-sunny-filled-loop-transition"
+    ? 'line-md:moon-twotone-loop'
+    : 'line-md:moon-filled-alt-to-sunny-filled-loop-transition'
 );
 </script>
 
 <template>
-  <Button
+  <VButton
     size="icon"
     variant="outline"
     class="text-foreground"
     @click="theme.toggleDarkMode()"
   >
     <icon :icon="themeIcon" width="24" height="24" />
-  </Button>
+  </VButton>
 </template>
